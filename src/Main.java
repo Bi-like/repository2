9570e72a230c4206ebffb2afc59c79780dc8b67f
Bi-like <v9100035777@gmail.com>
2023-11-26 16:21:29 +0300

    intial commit

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//
+//        int n = in.nextInt();
+//        int count = 0;
+//
+//        for (int i = 0; i < n; i++) {
+//            int number = in.nextInt();
+//            if (number % 3 == 0 && number %10 == 8) {
+//                count += number;
+//            }
+//        }
+//        System.out.println(count);
+//    }
+//}
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//
+//        int n = in.nextInt();
+//        int count = 0;
+//
+//        for (int i = 0; i < n; i++) {
+//            int number = in.nextInt();
+//            if (number % 5 == 0) {
+//                count += number;
+//            }
+//        }
+//        System.out.println(count);
+//    }
+//}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int sum = 0;
+        int countPositives = 
+
+
+        for (int i = 0; i < n; i++) {
+            int number = in.nextInt();
+            sum += number;
+        }
+
+        double t = (double) sum / n;
+        System.out.println(t);
+
+        if (){
+
+        }
+    }
+}
